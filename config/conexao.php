e4af1c20f0d950751f188c9c8010a567438cd584
Laisa Larissa <GeronimoNeto>
2022-04-08 08:28:31 -0300

    first commit

---
+++ config/conexao.php
@@ -0,0 +1,20 @@
+<?php
+
+try {
+    DEFINE('HOST','localhost');
+    DEFINE('BD','bd_Agenda');
+    DEFINE('USER','root');
+    DEFINE('PASS','bdjmf');
+  
+    $conect = new PDO ('mysql:host='.HOST.';dbname='.BD,USER,PASS);
+    $conect -> setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+} catch (PDOException $e) {
+    echo "<strong>Erro</strong> ".$e->getMessage();
+}
+
+
+
+
+
+
